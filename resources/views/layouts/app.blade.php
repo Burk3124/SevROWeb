28d8318db30480735df124f8fdfdfc7940a35c81
YuraBobryshev <urabobrenok2005@gmail.com>
2026-01-09 19:27:06 +0300

    Добавили favicon используя логотип РСО

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'SevROWeb - Главная')</title>
-    <meta name="description" content="@yield('description', 'SevROWeb - современное веб-приложение с авторизацией через VK и Telegram')">
+    <title>@yield('title', 'РСО Севастополь - @yield('title', 'Главная')')</title>
+    <meta name="description" content="@yield('description', 'Российские студенческие отряды Севастополя - с нами ты можешь больше!')">
 
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/LogoSevRO.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/LogoSevRO.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/LogoSevRO.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <meta name="theme-color" content="#1d4ed8">
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
